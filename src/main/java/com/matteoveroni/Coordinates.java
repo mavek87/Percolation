eec18ade32e48c99d7ca3b3b50aefb8382a62c5f
matver87@gmail.com <>
2016-09-13 17:15:30 +0200

    third commit

---
+++ src/main/java/com/matteoveroni/Coordinates.java
@@ -0,0 +1,24 @@
+package com.matteoveroni;
+
+/**
+ *
+ * @author Matteo Veroni
+ */
+public class Coordinates {
+
+	private final int x;
+	private final int y;
+
+	public Coordinates(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+}
--- src/main/java/com/matteoveroni/Percolation.java
+++ src/main/java/com/matteoveroni/Percolation.java
@@ -1,5 +1,7 @@
 package com.matteoveroni;
 
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
 /**
  *
  * @author Matteo Veroni
@@ -22,92 +24,120 @@ package com.matteoveroni;
  */
 public class Percolation {
 
-	private final int[][] matrix;
+	private final int N;
+	private final int[][] sitesMatrix;
+	private final int[][] incrementalCoordinates;
 
-	private final int virtualTop = Integer.MAX_VALUE;
-	private final int virtualBottom = Integer.MIN_VALUE;
+	private final WeightedQuickUnionUF unionFind;
 
 	/**
 	 * Create n-by-n grid, with all sites blocked
 	 *
 	 * @param n squared grid dimensions
 	 */
-	public Percolation(int n) {
-		matrix = new int[n][n];
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++) {
-				matrix[i][j] = -1;
+	public Percolation(int n) throws IllegalArgumentException {
+		if (n <= 0) {
+			throw new IllegalArgumentException();
+		}
+		N = n;
+		sitesMatrix = new int[n + 1][n + 1];
+		incrementalCoordinates = new int[n + 1][n + 1];
+		int i = 0;
+		for (int y = 1; y < n + 1; y++) {
+			for (int x = 1; x < n + 1; x++) {
+				incrementalCoordinates[x][y] = i;
+				sitesMatrix[x][y] = -1;
+				System.out.print("(x=" + x + ",y=" + y + "):" + sitesMatrix[x][y] + "\t");
+				i++;
+			}
+			System.out.println("\n");
+		}
+		System.out.println("\n");
+		unionFind = new WeightedQuickUnionUF(n * n);
+	}
+
+	/**
+	 * Open site (row x, column y) if it is not
+	 *
+	 * @param x number of rows
+	 * @param y number of columns
+	 */
+	public void open(int x, int y) throws IllegalArgumentException {
+		checkIfCoordinatesAreInsideBounds(x, y);
+		if (!isOpen(x, y)) {
+			sitesMatrix[x][y] = incrementalCoordinates[x][y];
+
+			// TOP => RIGHT => BOTTOM => LEFT
+			final int xNeighbours[] = {x, x + 1, x, x - 1};
+			final int yNeighbours[] = {y + 1, y, y - 1, y};
+
+			for (int index = 0; index < 4; index++) {
+				try {
+					if (isOpen(xNeighbours[index], yNeighbours[index])) {
+//						int openNeighbourRootValue = unionFind.find();
+						System.out.println("a=> " + sitesMatrix[xNeighbours[index]][yNeighbours[index]]);
+						unionFind.union(sitesMatrix[x][y], sitesMatrix[xNeighbours[index]][yNeighbours[index]]);
+//						sitesMatrix[x][y] = openNeighbourRootValue;
+					}
+				} catch (Exception ex) {
+				}
 			}
 		}
+
+		printMatrix();
 	}
 
 	/**
-	 * Open site (row i, column j) if it is not
+	 * Is site (row x, column y) open?
 	 *
-	 * @param i number of rows
-	 * @param j number of columns
-	 */
-	public void open(int i, int j) {
-
-	}
-
-	/**
-	 * Is site (row i, column j) open?
-	 *
-	 * @param i row coordinate
-	 * @param j column coordinate
+	 * @param x row coordinate
+	 * @param y column coordinate
 	 * @return if the site is open or not
 	 */
-	public boolean isOpen(int i, int j) {
-
-		// aperto se ci sono altri full intorno
-		try {
-
-			boolean atLeastANeighbourIsOpen = false;
-
-			// DOWN
-			if (isFull(i, j - 1)) {
-				return true;
-			}
-
-			// UP
-			if (isFull(i, j + 1)) {
-				return true;
-			}
-
-			// RIGHT
-			if (isFull(i + 1, j)) {
-				return true;
-			}
-
-			// LEFT
-			if (isFull(i - 1, j)) {
-				return true;
-			}
-
-		} catch (Exception ex) {
-		}
-
-		return false;
+	public boolean isOpen(int x, int y) throws IllegalArgumentException {
+		checkIfCoordinatesAreInsideBounds(x, y);
+		return sitesMatrix[x][y] >= 0;
 	}
 
 	/**
-	 * Is site (row i, column j) full?
+	 * Is site (row x, column y) full?
 	 *
-	 * @param i row coordinate
-	 * @param j column coordinate
+	 * @param x row coordinate
+	 * @param y column coordinate
 	 * @return if the site is full or not
 	 *
 	 * @throws IllegalArgumentException if row or column is out of bound
 	 */
-	public boolean isFull(int i, int j) throws IllegalArgumentException {
-		if (i < 0 || j < 0) {
-			throw new IllegalArgumentException();
+	public boolean isFull(int x, int y) throws IllegalArgumentException {
+		checkIfCoordinatesAreInsideBounds(x, y);
+		throw new UnsupportedOperationException();
+	}
+
+	private void checkIfCoordinatesAreInsideBounds(int x, int y) throws IndexOutOfBoundsException {
+		if (x < 1 || y < 1 || x > N || y > N) {
+			throw new IndexOutOfBoundsException();
 		}
-		return matrix[i][j] > 0;
 	}
 
 	public static void main(String[] args) {
+		Percolation percolation = new Percolation(7);
+//		WeightedQuickUnionUF unionFind = new WeightedQuickUnionUF(7);
+		percolation.open(1, 1);
+		percolation.open(1, 2);
+		percolation.open(4, 1);
+		percolation.open(4, 2);
+		percolation.open(4, 3);
+		percolation.open(4, 4);
+		percolation.open(6, 6);
+	}
 
+	private void printMatrix() {
+		for (int y = 1; y < N + 1; y++) {
+			for (int x = 1; x < N + 1; x++) {
+				System.out.print("(x=" + x + ",y=" + y + "):" + sitesMatrix[x][y] + "\t");
+			}
+			System.out.println("\n");
+		}
+		System.out.println("\n");
 	}
 }
